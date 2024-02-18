9881f3cb1f6505f3aebe7871a8908fe534b3449f
Kai Ishii <callofwar0627@gmail.com>
2024-02-18 16:16:24 +0900

    期限ごとに分けるリストを作った。1ページ目のデザインも完

--- ivol/ViewController.swift
+++ ivol/ViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var tableview: UITableView!
+    private var contents = ["Expired", "Tomorrow", "3 days", "1 week", "2 weeks", "1 month", "3 monthes", "More"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +18,17 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = contents[indexPath.row]
+        return cell
+    }
 }
 
